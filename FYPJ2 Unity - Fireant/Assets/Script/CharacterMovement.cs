7923870c9883669502df4a4782052c989038d3ed
YongHan <powerdynamic1@gmail.com>
2018-05-02 12:08:50 +0800

    Y THE CHARACTER DOESNT MOVE PART 2

--- FYPJ2 Unity - Fireant/Assets/Script/CharacterMovement.cs
+++ FYPJ2 Unity - Fireant/Assets/Script/CharacterMovement.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+    int movementspeed;
 	void Start () {
-		
+        movementspeed = 10;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.up * movementspeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * movementspeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * movementspeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.up * movementspeed * Time.deltaTime;
+        }
+    }
 }
