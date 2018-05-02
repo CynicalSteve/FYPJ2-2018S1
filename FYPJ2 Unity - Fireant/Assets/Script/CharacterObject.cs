c7480d412fcec83cd7918683ca7d2be1749e218f
CynicalSteve <syong789@hotmail.com>
2018-05-02 15:31:41 +0800

    -Basic Character movement -Integrated basic movement into main scene

---
+++ FYPJ2 Unity - Fireant/Assets/Script/CharacterObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterObject : MonoBehaviour {
+
+    [SerializeField]
+    Image theCharacter;
+
+    [SerializeField]
+    uint characterMovementSpeed = 10;
+
+    GeneralMovement generalMovementScript = new GeneralMovement();
+
+    // Use this for initialization
+    void Start()
+    {
+        generalMovementScript = GameObject.FindGameObjectWithTag("GeneralScripts").GetComponent<GeneralMovement>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += transform.up * characterMovementSpeed * Time.deltaTime;
+            generalMovementScript.moveUp(theCharacter, characterMovementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.position += transform.right * characterMovementSpeed * Time.deltaTime;
+            generalMovementScript.moveDown(theCharacter, characterMovementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //transform.position -= transform.right * characterMovementSpeed * Time.deltaTime;
+            generalMovementScript.moveLeft(theCharacter, characterMovementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //transform.position -= transform.up * characterMovementSpeed * Time.deltaTime;
+            generalMovementScript.moveRight(theCharacter, characterMovementSpeed);
+        }
+    }
+}
---
+++ FYPJ2 Unity - Fireant/Assets/Script/GeneralMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneralMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+	}
+
+    public void moveUp(Image theImage, uint movementSpeed)
+    {
+        theImage.transform.position += transform.up * movementSpeed * Time.deltaTime;
+    }
+
+    public void moveDown(Image theImage, uint movementSpeed)
+    {
+        theImage.transform.position -= theImage.transform.up * movementSpeed * Time.deltaTime;
+    }
+
+    public void moveLeft(Image theImage, uint movementSpeed)
+    {
+        theImage.transform.position -= theImage.transform.right * movementSpeed * Time.deltaTime;
+    }
+
+    public void moveRight(Image theImage, uint movementSpeed)
+    {
+        theImage.transform.position += theImage.transform.right * movementSpeed * Time.deltaTime;
+    }
+}
