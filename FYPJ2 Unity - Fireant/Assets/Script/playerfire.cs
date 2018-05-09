513146c1aeaf5b2b28d8f651fe94e90653ad3e5d
YongHan <powerdynamic1@gmail.com>
2018-05-09 11:49:02 +0800

    MY THINGS HVNT DONE YET

---
+++ FYPJ2 Unity - Fireant/Assets/Script/playerfire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerfire : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bulletObj;//points to bullet pref
+                         // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletObj, transform.position, transform.rotation);
+        }
+    }
+}
